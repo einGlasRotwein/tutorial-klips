c1ad672e66ebf151e11cd51e90518a7173a93629
einGlasRotwein <juliane.tkotz@gmail.com>
2019-12-02 13:35:09 +0100

    Added R script to show how you can push files in multiple commits.

---
+++ example_script.R
@@ -0,0 +1,10 @@
+
+##### EXAMPLE R SCRIPT ####
+# This is an example R script illustrating how to push new files. It doesn't do anything,
+# but here is a random reminder why you should always use TRUE instead of T:
+
+TRUE == TRUE
+TRUE == T
+
+T <- 42
+TRUE == T
